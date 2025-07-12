520f90e43f95a0bbfeb60d3590b19cd482cd8da2
Umang Hirani <umanghirani.exe@gmail.com>
2025-07-12 10:33:30 +0530

    basic routes set up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 import "./App.css";
 
 function App() {
-  return <>hello world</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
