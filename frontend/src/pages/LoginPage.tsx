fd22c8473d9b9c5d2e7039ea80cc927451155186
Umang Hirani <umanghirani.exe@gmail.com>
2025-07-12 15:14:24 +0530

    ui changes in signin and signup

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
       </div>
       <div className="bg-muted relative hidden lg:block">
         <img
-          src="/placeholder.svg"
+          src="https://images.unsplash.com/photo-1669299033175-09d02a0031f1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODh8fHN0YWNrfGVufDB8fDB8fHww"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
