f454026e07bdcf1f15abc19aaea04cfcf9c43d3f
Jay <115220083+Jaymangukiya22@users.noreply.github.com>
2025-07-12 11:28:07 +0530

    login-page completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
+import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
